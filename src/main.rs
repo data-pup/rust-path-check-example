9af81084a4a0b62371a668c9626aab6b0584309a
data-pup <kevinmartinucr@gmail.com>
2018-05-10 07:05:12 -0700

    Added an initial proof of concept for what the path check logic will look like.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// std::fs::Metadata.is_dir() will confirm that the path points to a directory.
+
 fn main() {
-    println!("Hello, world!");
+    let path_arg = std::env::args()
+        .skip(2)
+        .rev()
+        .next()
+        .unwrap_or(".".to_string());
+
+    let dir_metadata = std::fs::metadata(&path_arg).expect("Could not open path!");
+
+    match dir_metadata.is_dir() {
+        true => println!("Path argument '{}' is a directory! OuO", path_arg),
+        false => println!("Path argument '{}' is not a directory! OnO", path_arg),
+    }
 }
